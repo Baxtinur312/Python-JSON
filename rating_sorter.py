0b3c83f0ee93687b11f7a71c4175ba2a036ee8ac
Baxtinur312 <baxtinururolov95@gmail.com>
2025-07-03 16:25:37 +0500

    bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,8 +1,12 @@
 import json
 
 with open('students.json', 'r') as file:
- students = json.loads(file.read())
-# Sort students by rating in descending order
-students.sort(key=lambda x: x['score'], reverse=True)
-with open('rating.json', 'w') as file:
-    file.write(json.dumps(students, indent=4))
+    students = json.load(file)
+
+# Sort students by score in descending order
+sorted_students = sorted(students, key=lambda x: x['score'], reverse=True)
+ranked_students = [f"{i+1},{student['name']},{student['score']}\n" for i, student in enumerate(sorted_students)]
+
+with open('rating.csv', 'w') as f:
+    f.write('Rank,Name,Rating\n')
+    f.writelines(ranked_students)
