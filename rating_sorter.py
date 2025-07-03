fb715c4bb40ef54952aa7a607423aa54345c113a
Baxtinur312 <baxtinururolov95@gmail.com>
2025-07-03 16:19:59 +0500

    bajrildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,8 @@
+import json
 
+with open('students.json', 'r') as file:
+ students = json.loads(file.read())
+# Sort students by rating in descending order
+students.sort(key=lambda x: x['score'], reverse=True)
+with open('rating.json', 'w') as file:
+    file.write(json.dumps(students, indent=4))
